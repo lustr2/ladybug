3f444031f38ceac8f15f382bab9386e2dce78602
mvdegtyar <mv_degtyar@yahoo.com>
2024-04-24 22:36:13 +0300

    Upgrade project to use type script

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,40 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import Ladybug from './components/Ladybug';
+import { Direction } from './components/Ladybug';
 import './style.css';
 
 const STEP_SIZE = 25;
 
-const App = () => {
-  const [posX, setPosX] = useState(100);
-  const [posY, setPosY] = useState(100);
-  const [orientation, setOrientation] = useState('right');
-  
+const App: React.FC = () => {
+  const [posX, setPosX] = useState<number>(100);
+  const [posY, setPosY] = useState<number>(100);
+  const [orientation, setOrientation] = useState<Direction>(Direction.right);
+
   const handleKeyUp = ({ code }) => {
     if (code === 'ArrowUp') {
-      setOrientation('up');
+      setOrientation(Direction.up);
       setPosX(posX - STEP_SIZE);
     } else if (code === 'ArrowLeft') {
-      setOrientation('left');
+      setOrientation(Direction.left);
       setPosY(posY - STEP_SIZE);
     } else if (code === 'ArrowRight') {
-      setOrientation('right');
+      setOrientation(Direction.right);
       setPosY(posY + STEP_SIZE);
     } else if (code === 'ArrowDown') {
-      setOrientation('down');
+      setOrientation(Direction.down);
       setPosX(posX + STEP_SIZE);
     }
   };
 
   return (
-    <div 
+    <div
       tabIndex={-1}
       className="field"
       onKeyDown={handleKeyUp}
     >
       <header>Click anywhere to start the game</header>
-      <Ladybug posX={posX} posY={posY} orientation={orientation}/>
+      <Ladybug posX={posX} posY={posY} orientation={orientation} />
     </div>
   );
 };
